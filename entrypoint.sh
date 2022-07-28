a28260cc78a61b3c8ca5214e59ca3632ad5b2a9c
Emilio López <emilio.lopez@trailofbits.com>
2022-07-28 19:22:41 -0300

    Add a way to install Slither plugins
    This adds an extra option, `slither-plugins`, that lets users provide
    a requirements.txt file to be installed alongside Slither. This can
    be used to install Slither plugins.
    
    Fixes: #16

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,6 +21,7 @@ SARIFOUT="$4"
 SLITHERVER="$5"
 SLITHERARGS="$(get INPUT_SLITHER-ARGS)"
 SLITHERCONF="$(get INPUT_SLITHER-CONFIG)"
+SLITHERPLUGINS="$(get INPUT_SLITHER-PLUGINS)"
 STDOUTFILE="/tmp/slither-stdout"
 IGNORECOMPILE="$(get INPUT_IGNORE-COMPILE)"
 
@@ -185,6 +186,13 @@ install_slither()
     export PATH="/opt/slither/bin:$PATH"
     pip3 install wheel
     pip3 install "$SLITHERPKG"
+
+    if [[ -n "$SLITHERPLUGINS" ]]; then
+        echo "[-] Slither plugins provided, installing them"
+        pushd "$(dirname "$SLITHERPLUGINS")" >/dev/null
+        pip3 install -r "$(basename "$SLITHERPLUGINS")"
+        popd >/dev/null
+    fi
 }
 
 install_deps()
